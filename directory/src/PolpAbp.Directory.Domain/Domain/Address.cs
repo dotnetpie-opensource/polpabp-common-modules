815b204f1483b822618c789021c1162666664a11
dotnetpie <tom@fake.com>
2022-07-27 18:23:34 -0400

    k

---
+++ directory/src/PolpAbp.Directory.Domain/Domain/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Volo.Abp.Domain;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace PolpAbp.Directory.Domain
+{
+    public class Address : CreationAuditedEntity<Guid>
+    {
+    }
+}
+
---
+++ directory/src/PolpAbp.Directory.Domain/Domain/State.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PolpAbp.Directory.Domain
+{
+    public class State
+    {
+        public State()
+        {
+        }
+    }
+}
+
