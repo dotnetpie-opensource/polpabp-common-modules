a3b063e333bfe6800a45d6b04f0665f32c5d4d53
dotnetpie <tom@fake.com>
2022-07-28 13:03:35 -0400

    Missed files

---
+++ directory/src/PolpAbp.Directory.Domain.Shared/DirectoryDomainConsts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PolpAbp.Directory
+{
+    public static class DirectoryDomainConsts
+    {
+        public const int MaxCountryNameLength = 128;
+        public const int MaxStateProvinceNameLength = 128;
+
+        public const int MaxFirstNameLength = 256;
+        public const int MaxLastNameLength = 256;
+
+        public const int MaxStreetNameLength = 256;
+
+        public const int MaxEmailLength = 256;
+
+        public const int MaxCityLength = 128;
+        public const int MaxPhoneCountryCodeLength = 8;
+        public const int MaxPhoneNumberLength = 32;
+    }
+}
+
