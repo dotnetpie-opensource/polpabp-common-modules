53f791cfa3da4079c86c2853d2de04430fc0f072
Tom Tang <polpware@gmail.com>
2023-03-29 21:37:12 -0400

    Fix tests

--- res/test/PolpAbp.ResourceManagement.Application.Tests/Services/SmsResourceLogContributor.cs
+++ res/test/PolpAbp.ResourceManagement.Application.Tests/Services/SmsResourceLogContributor.cs
@@ -1,10 +1,20 @@
-﻿using Volo.Abp.DependencyInjection;
+﻿using PolpAbp.ResourceManagement.Domain.Entities;
+using System;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
 
 namespace PolpAbp.ResourceManagement.Services
 {
     [ExposeServices(typeof(SmsResourceLogContributor))]
     public class SmsResourceLogContributor : ResourceLogContributor, ITransientDependency
     {
+        public SmsResourceLogContributor(IRepository<ResourceUsageLog, Guid> usageLogRepo, 
+            IRepository<ResourceMonthlyUsage, Guid> monthlyUsageRepo, 
+            IRepository<ResourceYearlyUsage, Guid> yearlyUsageRepo) 
+            : base(usageLogRepo, monthlyUsageRepo, yearlyUsageRepo)
+        {
+        }
+
         public override string ResourceName => ResourceManagementTestConsts.SmsResourceName;
     }
 }
