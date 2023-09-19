556032c91e3b7244f824b9d79cb8a6dfdd05d7bc
Tom Tang <polpware@gmail.com>
2023-09-19 07:59:40 -0400

    Updated unit tests

--- res/test/PolpAbp.ResourceManagement.Application.Tests/Services/SubscriptionPlanServiceTests.cs
+++ res/test/PolpAbp.ResourceManagement.Application.Tests/Services/SubscriptionPlanServiceTests.cs
@@ -30,7 +30,7 @@ namespace PolpAbp.ResourceManagement.Services
         }
 
         [Fact]
-        public async Task CanGetQuotaAsync()
+        public async Task CanGetQuota4NonNumberedPlanAsync()
         {
             using (_currentTenant.Change(ResourceManagementTestConsts.TenantId))
             {
@@ -63,6 +63,7 @@ namespace PolpAbp.ResourceManagement.Services
                         IsTerminated = false,
                         EffectiveOn = DateTime.UtcNow.AddDays(-1),
                         PlanId = plan.Id,
+                        Quantity = 1,
                         TenantId = ResourceManagementTestConsts.TenantId
                     };
 
@@ -75,6 +76,63 @@ namespace PolpAbp.ResourceManagement.Services
                     .GetQuotaAsync(ResourceManagementTestConsts.SmsResourceName, true, default);
 
                     Assert.Equal(10000, tenantQuota);
+
+                    var userQuota = await _subscriptionPlanService
+                    .GetQuotaAsync(ResourceManagementTestConsts.SmsResourceName, false, default);
+
+                    Assert.Equal(1000, userQuota);
+
+                });
+            }
+        }
+
+        [Fact]
+        public async Task CanGetQuota4NumberedPlanAsync()
+        {
+            using (_currentTenant.Change(ResourceManagementTestConsts.TenantId))
+            {
+                await WithUnitOfWorkAsync(async () =>
+                {
+                    var resource = await _resourceRepository
+                    .GetAsync(x => x.Name == ResourceManagementTestConsts.SmsResourceName);
+
+                    var plan = new Plan(Guid.NewGuid())
+                    {
+                        Name = "Per person Plan",
+                        Description = "Description",
+                        BillingCycleId = (int)BillingCycleEnum.Month,
+                        Breakdowns = new List<PlanBreakdown>
+                        {
+                            new PlanBreakdown(Guid.NewGuid())
+                            {
+                               ResourceId = resource.Id,
+                               LimitPerUser = 1000,
+                               LimitAcrossTenant = 10000
+                            }
+                        }
+                    };
+
+                    await _planRepository.InsertAsync(plan);
+
+                    var sub = new TenantSubscription(Guid.NewGuid())
+                    {
+                        BillingCycleOn = DateTime.UtcNow.AddDays(-1),
+                        IsTerminated = false,
+                        EffectiveOn = DateTime.UtcNow.AddDays(-1),
+                        PlanId = plan.Id,
+                        Quantity = 10,
+                        TenantId = ResourceManagementTestConsts.TenantId
+                    };
+
+                    await _tenantSubscriptionRepository.InsertAsync(sub);
+                });
+
+                await WithUnitOfWorkAsync(async () =>
+                {
+                    var tenantQuota = await _subscriptionPlanService
+                    .GetQuotaAsync(ResourceManagementTestConsts.SmsResourceName, true, default);
+
+                    Assert.Equal(10000 * 10, tenantQuota);
 
                     var userQuota = await _subscriptionPlanService
                     .GetQuotaAsync(ResourceManagementTestConsts.SmsResourceName, false, default);
