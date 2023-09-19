c5511ffb0192a58c6139ea0ba8c62f13b1823fb2
dotnetpie <tom@fake.com>
2023-09-19 10:22:56 -0400

    Comment

--- res/src/PolpAbp.ResourceManagement.Application.Contracts/Services/ISubscriptionPlanService.cs
+++ res/src/PolpAbp.ResourceManagement.Application.Contracts/Services/ISubscriptionPlanService.cs
@@ -10,6 +10,12 @@ namespace PolpAbp.ResourceManagement.Services
     {
         Task<Tuple<DateTime, DateTime?>> GetCurrentBillingPeriodAsync(string resourceName, CancellationToken cancellationToken);
         Task<long> GetQuotaAsync(string resourceName, bool isTenantLevel, CancellationToken cancellationToken);
+        /// <summary>
+        /// Computes the effective plans for now or any future time
+        /// </summary>
+        /// <param name="referenceTime">The reference time when the plans will be effective</param>
+        /// <param name="cancellationToken">Token</param>
+        /// <returns>List of plans</returns>
         Task<List<SubscriptionPlanOutputDto>> LoadEffectivePlansAsync(DateTime referenceTime, CancellationToken cancellationToken);
     }
 }
