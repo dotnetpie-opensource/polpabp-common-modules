873685ca57ba407da898d5da153458117a29d9d1
Tom T <polpware@gmail.com>
2024-10-31 08:06:31 -0400

    Start to impl message templates

---
+++ msgtmpl/src/PolpAbp.MessageTemplates.Domain.Shared/MessageTemplatesDomainSharedModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+
+namespace PolpAbp.MessageTemplates;
+
+[DependsOn(
+    typeof(AbpValidationModule)
+)]
+public class MessageTemplatesDomainSharedModule : AbpModule
+{
+
+}
---
+++ msgtmpl/src/PolpAbp.MessageTemplates.Domain/MessageTemplatesDomainModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Domain;
+using Volo.Abp.Modularity;
+
+namespace PolpAbp.MessageTemplates;
+
+[DependsOn(
+    typeof(AbpDddDomainModule),
+    typeof(MessageTemplatesDomainSharedModule)
+)]
+public class MessageTemplatesDomainModule : AbpModule
+{
+
+}
